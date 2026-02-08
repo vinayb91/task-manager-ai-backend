27f8b4761594d615e0241be7e1e5bc2a1d333361
Vinay B <bvinay9198@gmail.com>
2026-02-08 12:13:57 +0530

    Add register, login and authResponse struct models

--- internal/models/models.go
+++ internal/models/models.go
@@ -11,6 +11,22 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
+type RegisterRequest struct {
+	Email    string `json:"email"`
+	Name     string `json:"name"`
+	Password string `json:"password"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type AuthResponse struct {
+	Token string `json:"token"`
+	User  User   `json:"user"`
+}
+
 // Domain Models
 type Task struct {
 	ID          int       `json:"id"`
